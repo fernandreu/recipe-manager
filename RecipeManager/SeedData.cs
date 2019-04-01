774df7cae940df6bb414da21600169d19be13279
Fernando <2480661+fernandreu@users.noreply.github.com>
2019-04-01 22:21:39 +0100

    Revert back SeedData changes
    In the end, they were not needed to fix lazy properties

--- RecipeManager/SeedData.cs
+++ RecipeManager/SeedData.cs
@@ -31,123 +31,116 @@ namespace RecipeManager
                 // Already has data
                 return;
             }
-
-            var recipes = new[]
-            {
-                new RecipeEntity
-                {
-                    Id = Guid.NewGuid(),
-                    Title = "Spanish Omelet",
-                    Ingredients = new[]
-                    {
-                        new IngredientEntity
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "6 eggs",
-                        },
-                        new IngredientEntity
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "2kg potatoes",
-                        },
-                        new IngredientEntity
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "1 onion",
-                        },
-                    },
-                    Details = "Peel and slice the potatoes and fry them in a saucepan together with mixed eggs.",
-                },
-                new RecipeEntity
-                {
-                    Id = Guid.NewGuid(),
-                    Title = "French Omelet",
-                    Ingredients = new[]
-                    {
-                        new IngredientEntity
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "3 eggs",
-                        },
-                        new IngredientEntity
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "Salt",
-                        },
-                        new IngredientEntity
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "Oil",
-                        },
-                    },
-                    Details = "Beat the eggs, put them in a pan with a bit of oil and a pinch of salt, and wait.",
-                },
-                new RecipeEntity
-                {
-                    Id = Guid.NewGuid(),
-                    Title = "Carrot Cake",
-                    Ingredients = new[]
-                    {
-                        new IngredientEntity
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "6 carrots",
-                        },
-                        new IngredientEntity
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "Muscovado sugar",
-                        },
-                        new IngredientEntity
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "300g flour",
-                        },
-                        new IngredientEntity
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "2 eggs",
-                        },
-                    },
-                    Details = "Mix everything together and put it into the oven somehow.",
-                },
-                new RecipeEntity
-                {
-                    Id = Guid.NewGuid(),
-                    Title = "Spinach and ricotta cannelloni",
-                    Ingredients = new[]
-                    {
-                        new IngredientEntity
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "500g spinach",
-                        },
-                        new IngredientEntity
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "300g ricotta",
-                        },
-                        new IngredientEntity
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "12 cannelloni tubes",
-                        },
-                        new IngredientEntity
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "300g mozzarella",
-                        },
-                    },
-                    Details = "Mix everything together and put it into the oven somehow.",
-                },
-            };
             
-            foreach (var recipe in recipes)
+            context.Recipes.Add(new RecipeEntity
             {
-                context.Ingredients.AddRange(recipe.Ingredients);
-            }
+                Id = Guid.NewGuid(),
+                Title = "Spanish Omelet",
+                Ingredients = new[]
+                {
+                    new IngredientEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "6 eggs",
+                    },
+                    new IngredientEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "2kg potatoes",
+                    },
+                    new IngredientEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "1 onion",
+                    },
+                },
+                Details = "Peel and slice the potatoes and fry them in a saucepan together with mixed eggs.",
+            });
 
-            context.Recipes.AddRange(recipes);
+            context.Recipes.Add(new RecipeEntity
+            {
+                Id = Guid.NewGuid(),
+                Title = "French Omelet",
+                Ingredients = new[]
+                {
+                    new IngredientEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "3 eggs",
+                    },
+                    new IngredientEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "Salt",
+                    },
+                    new IngredientEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "Oil",
+                    },
+                },
+                Details = "Beat the eggs, put them in a pan with a bit of oil and a pinch of salt, and wait.",
+            });
+
+            context.Recipes.Add(new RecipeEntity
+            {
+                Id = Guid.NewGuid(),
+                Title = "Carrot Cake",
+                Ingredients = new[]
+                {
+                    new IngredientEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "6 carrots",
+                    },
+                    new IngredientEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "Muscovado sugar",
+                    },
+                    new IngredientEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "300g flour",
+                    },
+                    new IngredientEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "2 eggs",
+                    },
+                },
+                Details = "Mix everything together and put it into the oven somehow.",
+            });
+
+            context.Recipes.Add(new RecipeEntity
+            {
+                Id = Guid.NewGuid(),
+                Title = "Spinach and ricotta cannelloni",
+                Ingredients = new[]
+                {
+                    new IngredientEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "500g spinach",
+                    },
+                    new IngredientEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "300g ricotta",
+                    },
+                    new IngredientEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "12 cannelloni tubes",
+                    },
+                    new IngredientEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "300g mozzarella",
+                    },
+                },
+                Details = "Mix everything together and put it into the oven somehow.",
+            });
 
             await context.SaveChangesAsync();
         }
