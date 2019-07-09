2c9dbee4524f62a1c7fe42f1ed5b25d5b2f0b7c4
Fernando <2480661+fernandreu@users.noreply.github.com>
2019-07-10 00:07:36 +0100

    Add User entity
    This has a one-to-many relationship with Ingredient
    entities, which might be a problem as Recipe also does

---
+++ src/ApplicationCore/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using RecipeManager.ApplicationCore.Attributes;
+
+namespace RecipeManager.ApplicationCore.Entities
+{
+    public class User : BaseEntity
+    {
+        public string UserName { get; set; }
+
+        [SearchableIngredients]
+        public ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
--- src/Infrastructure/Data/RecipeApiDbContext.cs
+++ src/Infrastructure/Data/RecipeApiDbContext.cs
@@ -14,5 +14,7 @@ namespace RecipeManager.Infrastructure.Data
         public DbSet<Recipe> Recipes { get; set; }
 
         public DbSet<Ingredient> Ingredients { get; set; }
+
+        public DbSet<User> Users { get; set; }
     }
 }
