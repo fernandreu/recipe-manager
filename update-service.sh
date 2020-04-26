a7dbfe632b826d8a085438453aecd27f077b91e5
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2020-04-26 12:19:42 +0100

    Add script to update service in device

---
+++ update-service.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Variables needed for this:
+BUILD_ID=$1
+ref="$BUILD_ID"
+folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+serviceName=recipe-manager-api
+refPath="${folder}/${ref}"
+newLine="ExecStart=dotnet\ ${refPath}"
+echo New line is: ${newLine}
+if [[ -z "$newLine" ]]; then
+    echo "Wrong newLine; aborting"
+    exit 1
+fi
+
+downloadArtifact () {
+    artifactName=$1
+    destination=$2
+    apiUrl="https://dev.azure.com/fernandreu-public/RecipeManager/_apis/build/builds/${ref}/artifacts?artifactName=${artifactName}&api-version=4.1"
+    downloadUrl=$(curl -s ${apiUrl} | grep -Pom 1 '"downloadUrl": *"\K[^"]*')
+    downloadPath="/tmp/artifact-${artifactName}.zip"
+    wget -O ${downloadPath} ${downloadUrl}
+    artifactPath="/tmp/${artifactName}"
+    rm -rf ${artifactPath}
+    unzip ${downloadPath} -d "/tmp"
+    cp -rf "${artifactPath}/." ${destination}
+    
+    # Remove downloaded / temporarily extracted files
+    rm -rf ${artifactPath} 
+    rm -rf ${downloadPath}
+}
+
+# Re-run database
+downloadArtifact "DockerCompose" ${folder}
+composerPath="${folder}/docker-compose.yml"
+docker-compose -f ${composerPath} down
+docker-compose -f ${composerPath} up -d
+
+# Download binaries
+mkdir ${refPath}
+downloadArtifact "WebAPI" ${refPath}
+
+# Get the current deployment path from the service
+serviceFile="/etc/systemd/system/${serviceName}.service"
+line=$(sed -n -e '/^ExecStart=/p' ${serviceFile})
+previousPath=$(echo "$line" | awk '{print $2 }')
+echo Previous source code detected in: ${previousPath}
+
+# Replace that path with the new one
+echo New line is: ${newLine}
+escaped=$(sed -e 's/[\/&]/\\&/g' <<< ${newLine})
+echo Escaped: ${escaped}
+sudo sed -i \"/ExecStart=/c\\${escaped}\" ${serviceFile}
+
+# Restart service
+echo Restarting service
+sudo systemctl daemon-reload
+sudo systemctl restart ${serviceName}
+
+# Delete previous source code
+echo Deleting previous binaries in: ${previousPath}
+sudo rm -rf ${previousPath}
