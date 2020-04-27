164c645373907282359b59cd9c590ac422d3e469
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2020-04-27 20:42:23 +0100

    Hard-code server urls (for now)

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -18,6 +18,7 @@ namespace RecipeManager.WebApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://*:5000;https://*:5001");
     }
 }
