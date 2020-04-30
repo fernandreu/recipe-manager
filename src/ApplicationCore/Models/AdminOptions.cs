e8d44c06a6728860f0a0bd004387f1b8f13edc57
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2020-04-30 22:22:45 +0100

    Add admin page with reset command

---
+++ src/ApplicationCore/Models/AdminOptions.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AdminOptions.cs" company="MasterChefs">
+//   {{Copyright}}
+// </copyright>
+// <summary>
+//   Created by Fernando Andreu on 30/04/2020.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RecipeManager.ApplicationCore.Models
+{
+    public class AdminOptions
+    {
+        public string? Password { get; set; }
+    }
+}
